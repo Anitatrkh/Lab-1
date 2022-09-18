b3968c1e7fef78336e95a1ec88e1f148a040edd7
anita <anita.trkh@gmail>
2022-09-17 21:44:57 -0400

    lab one

---
+++ Project/Lab1/Program.cs
@@ -0,0 +1,242 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+//FullName: Anita Torki
+//Date: 09-15-2022
+using System;
+namespace lab
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+        // //1
+        // int principle = 1000;
+        // double interestRate = 0.0625;
+        // int numOfYears1 = 5;//profit 5years
+        // int numOfYears2 = 8;//profit 8years
+        // Console.WriteLine(principle * interestRate * numOfYears1);
+        // Console.WriteLine(principle * interestRate * numOfYears2);
+
+
+        //2
+        //  Console.Write("Please insert the length of a rectangle!");
+        // int first = Convert.ToInt32(Console.ReadLine());
+
+        // Console.Write("Please insert the breadth of a rectangle!");
+        // int second = Convert.ToInt32(Console.ReadLine());
+     
+        //     if (first == second )
+        //     {
+        //         Console.WriteLine("Square");
+        //     }
+        //     else if (first != second);
+        //     {
+        //         Console.WriteLine("Rectangle");
+        //     }
+        //     else
+        //     {
+        //         Console.WriteLine("Not a square nor a rectangle");
+        //     }
+
+        //     Console.ReadKey();
+
+        //3
+            // Console.WriteLine("Please write grading your school! ");
+            // int grade = Convert.ToInt32(Console.ReadLine());
+            // if ( grade < 25)
+            // {
+            //     Console.WriteLine("F");
+            // }
+            // else if (grade > 25  && grade < 45)
+            // {
+            //     Console.WriteLine("E");
+            // }
+            // else if (grade > 45  && grade < 50)
+            // {
+            //     Console.WriteLine("D");
+            // }
+            // else if (grade > 50  && grade < 60)
+            // {
+            //     Console.WriteLine("C");
+            // }
+            // else if (grade > 60  && grade < 80)
+            // {
+            //     Console.WriteLine("B");
+            // }
+            // else if (grade > 80 )
+            // {
+            //     Console.WriteLine("A");
+            // }
+
+            //4
+            // int a;
+            // int b;
+    
+
+            // Console.Write("Please write the first number!");
+            // a = Convert.ToInt32(Console.ReadLine());
+
+            // Console.Write("Please write the second number!");
+            // b = Convert.ToInt32(Console.ReadLine());
+            // if (a > b)
+            // {
+            //   Console.WriteLine("The first input is Greatest number");  
+            // }
+            // else
+            // {
+            // Console.WriteLine("The second input is the Greatest  Number ");
+            // }
+            // Console.ReadLine();
+
+            //5
+            // int a, b, c;
+            // double d, x1, x2;
+
+            // Console.WriteLine("Please calculate root of Quadratic Equation:");
+          
+
+            // Console.WriteLine(" Please write the number of a");
+            // a = Convert.ToInt32(Console.ReadLine());
+
+            // Console.WriteLine("Please write the number of b");
+            // b = Convert.ToInt32(Console.ReadLine());
+
+            // Console.WriteLine("Please write the number of c");
+            // c = Convert.ToInt32(Console.ReadLine());
+
+            // d=b*b-4*a*c;
+            
+	        // x1=(-b+Math.Sqrt(d))/(2*a);
+	        // x2=(-b-Math.Sqrt(d))/(2*a);
+
+	        // Console.WriteLine("the First Root1",x1);
+	        // Console.WriteLine("the Second root2",x2);
+
+
+            //6
+            // int a = 1;
+            // int b = 1;
+            // int c = 0;
+
+            // Console.WriteLine(a);
+            // Console.WriteLine(b);
+            // while (c < 101)
+            // {
+            //     c = a + b;
+            //     Console.WriteLine(c);
+            //     a = b;
+            //     b = c;
+            
+            // }
+
+
+            //7
+            // Console.WriteLine("Enter number1");
+            // double Number1 = Convert.ToDouble(Console.ReadLine());
+            // Console.WriteLine("Enter number2");
+            // double Number2 = Convert.ToDouble(Console.ReadLine());
+            // if (Math.Abs(Number1 - Number2) < 0.001)
+            // {
+            //     Console.WriteLine("They are the same up to three decimal places");
+            // }
+            // else
+            // {
+            //     Console.WriteLine("The are not the same up to three decimal places");
+            // }
+
+            //8
+            // Console.WriteLine("Please write the temperature in Fahrenheit!");
+            // double fahrenheit = Convert.ToDouble(Console.ReadLine());
+            // double celsius = (fahrenheit - 32) * 5 / 9;
+            // Console.WriteLine("The temperature in Celsius is:" + celsius);
+            
+            //9
+            // Console.WriteLine("Please write the number of minutes");
+            // int minutes=Convert.ToInt32(Console.ReadLine());
+            // int years = minutes /(365 * 24 *60);
+            // int days = minutes / (1440);
+            // Console.WriteLine("the number of years{years} and the number of days {days}:");
+
+
+            //10
+            // Console.WriteLine("Please write the a number:");
+            // double a = Convert.ToDouble(Console.ReadLine());
+
+            // Console.WriteLine("Please write the b number:");
+            // double b = Convert.ToDouble(Console.ReadLine());
+
+            // Console.WriteLine("Please write the c number:");
+            // double c = Convert.ToDouble(Console.ReadLine());
+
+            // double ave = (a + b + c) / 3;
+            // Console.WriteLine("The average of the three numbers is:" + ave);
+
+
+
+            //11
+            // Console.WriteLine("* * * * * * ==================================");
+            // Console.WriteLine(" * * * * *  ==================================");
+            // Console.WriteLine("* * * * * * ==================================");
+            // Console.WriteLine(" * * * * *  ==================================");
+            // Console.WriteLine("* * * * * * ==================================");
+            // Console.WriteLine(" * * * * *  ==================================");
+            // Console.WriteLine("* * * * * * ==================================");
+            // Console.WriteLine(" * * * * *  ==================================");
+            // Console.WriteLine("* * * * * * ==================================");
+            // Console.WriteLine("==============================================");
+            // Console.WriteLine("==============================================");
+            // Console.WriteLine("==============================================");
+            // Console.WriteLine("==============================================");
+            // Console.WriteLine("==============================================");
+            // Console.WriteLine("==============================================");
+
+            //12
+            // Console.WriteLine("Please write the length of the rectangle");
+            // double length = Convert.ToDouble(Console.ReadLine());
+
+            // Console.WriteLine("Please write the Width of the rectangle");
+            // double Width = Convert.ToDouble(Console.ReadLine());
+
+            // double area = length * Width;
+            // double perimeter = 2 * (length + Width);
+
+            // Console.WriteLine("The area of the rectangle is:" + area);
+            // Console.WriteLine("The perimeter of the rectangle is: " + perimeter);
+
+            //13
+            // Console.WriteLine(" +\"\"\"\"\"+");
+            // Console.WriteLine("[| o o |]");
+            // Console.WriteLine(" |  ^  |");
+            // Console.WriteLine(" | '-' |");
+            // Console.WriteLine(" +-----+");
+
+            //14
+                // Console.WriteLine("please write the number!");
+                // int Num = Convert.ToInt32(Console.ReadLine());
+                // if (Num >= 0)
+                // {
+                //         Console.WriteLine("This is a positive number:" + Num);
+                // }
+                // else
+                // {
+                //     Console.WriteLine("This is a negative number:" + Num);
+                // }
+
+            //15
+                
+                //  Console.WriteLine("please write the number!");
+                // int Num = Convert.ToInt32(Console.ReadLine());
+                
+                // if(Num  % 2 == 0)
+                // {
+                //         Console.WriteLine("This is a even number");
+                // }
+                // else
+                // {
+                //     Console.WriteLine("This is a add number");
+                // }
+                    
+            
+        }
+    }
+}
